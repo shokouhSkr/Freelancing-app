5098bc424cdd1f5e9a38eb0f77ff4574c6a7758d
shokouhSkr <shokouhskr@gmail.com>
2023-12-07 00:12:42 +0330

    added custom colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,15 @@
 import type { Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
+import { RecursiveKeyValuePair } from "tailwindcss/types/config";
+
+function withOpacity(variableName: string): string | RecursiveKeyValuePair<string, string> {
+  return ({ opacityValue }: { opacityValue?: number }) => {
+    if (opacityValue !== undefined) {
+      return `rgba(var(${variableName}), ${opacityValue})`;
+    }
+    return `rgb(var(${variableName}))`;
+  };
+}
 
 const config: Config = {
   content: [
@@ -9,6 +19,38 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        primary: {
+          900: withOpacity("--color-primary-900"),
+          800: withOpacity("--color-primary-800"),
+          700: withOpacity("--color-primary-700"),
+          600: withOpacity("--color-primary-600"),
+          500: withOpacity("--color-primary-500"),
+          400: withOpacity("--color-primary-400"),
+          300: withOpacity("--color-primary-300"),
+          200: withOpacity("--color-primary-200"),
+          100: withOpacity("--color-primary-100"),
+        },
+        secondary: {
+          900: withOpacity("--color-secondary-900"),
+          800: withOpacity("--color-secondary-800"),
+          700: withOpacity("--color-secondary-700"),
+          600: withOpacity("--color-secondary-600"),
+          500: withOpacity("--color-secondary-500"),
+          400: withOpacity("--color-secondary-400"),
+          300: withOpacity("--color-secondary-300"),
+          200: withOpacity("--color-secondary-200"),
+          100: withOpacity("--color-secondary-100"),
+          50: withOpacity("--color-secondary-50"),
+          0: withOpacity("--color-secondary-0"),
+        },
+        success: withOpacity("--color-success"),
+        warning: withOpacity("--color-warning"),
+        error: withOpacity("--color-error"),
+      },
+      container: {
+        center: true,
+      },
       fontFamily: {
         sans: ["Vazir", ...fontFamily.sans],
       },
