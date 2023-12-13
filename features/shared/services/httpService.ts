cfb700fca032d86b51130db8ab99dbc97eca5638
shokouhSkr <shokouhskr@gmail.com>
2023-12-13 23:36:37 +0330

    handled 401 error in axios

--- features/shared/services/httpService.ts
+++ features/shared/services/httpService.ts
@@ -1,9 +1,40 @@
 import axios from "axios";
 
+const BASE_URL = "http://localhost:5000/api";
+
 const apiManager = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: BASE_URL,
   withCredentials: true,
 });
+
+apiManager.interceptors.request.use(
+  (res) => res,
+  (err) => Promise.reject(err)
+);
+
+apiManager.interceptors.response.use(
+  (res) => res,
+  async (err) => {
+    console.log("err.config: ", err.config);
+    const originalConfig = err.config;
+
+    if (err.response.status === 401 && !originalConfig._retry) {
+      originalConfig._retry = true;
+
+      try {
+        const { data } = await axios.get(`${BASE_URL}/user/refresh-token`, {
+          withCredentials: true,
+        });
+
+        if (data) return apiManager(originalConfig);
+      } catch (err) {
+        return Promise.reject(err);
+      }
+    }
+
+    return Promise.reject(err);
+  }
+);
 
 const http = {
   get: apiManager.get,
